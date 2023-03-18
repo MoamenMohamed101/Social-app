9db065af62540491e7b97b0a073bc80ba88e80c3
Iway <mmoamenmohamed101@gmail.com>
2023-03-18 10:59:02 +0200

    social_app

--- lib/layout/social_layout.dart
+++ lib/layout/social_layout.dart
@@ -10,11 +10,43 @@ class SocialLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (BuildContext context) => SocialCubit()..getUserData(),
-      child: BlocConsumer<SocialCubit , SocialStates>(
+      child: BlocConsumer<SocialCubit, SocialStates>(
         builder: (BuildContext context, state) {
-          return const Scaffold();
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              elevation: 0.0,
+              title: const Text('New Feed'),
+            ),
+            body: Column(
+              children: [
+                Container(
+                  color: Colors.amber.withOpacity(.6),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        const Icon(Icons.info_outline),
+                        const SizedBox(width: 20,),
+                        const Expanded(
+                          child: Text('Please verify your email'),
+                        ),
+                        MaterialButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'SEND',
+                            style: TextStyle(color:  Colors.blue,fontSize: 20),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
         },
-      listener: (BuildContext context, Object? state) {  },
+        listener: (BuildContext context, Object? state) {},
       ),
     );
   }
---
+++ lib/logic_notes.dart
@@ -0,0 +1,6 @@
+// 117. Firebase Firestore
+//1- To send data to Cloud Firestore use .set and put in it toMap
+
+
+// 118. Send Email Verification
+//1- To Get data From Cloud Firestore use .get and make .then and put in it fromjson in object of model class
