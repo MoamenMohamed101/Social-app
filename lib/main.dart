256d9b96a355e25fc0a17b2ab13e531671fcbd3c
Iway <mmoamenmohamed101@gmail.com>
2023-04-10 08:36:14 +0200

    social_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_app/layout/cubit/cubit.dart';
@@ -13,6 +14,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   CacheHelper.init();
   await Firebase.initializeApp();
+  var token = await FirebaseMessaging.instance.getToken();
+  debugPrint('token: $token');
+  FirebaseMessaging.onMessage.listen((event) {
+    debugPrint(event.data.toString());
+    //debugPrint(event.notification!.body);
+  });
   Bloc.observer = MyBlocObserver();
   Widget? widget;
   uId = await CacheHelper.getData(key: 'uId');
