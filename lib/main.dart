6ab58e60fc79577eb09cc696c328c86505ddf466
Iway <mmoamenmohamed101@gmail.com>
2023-04-11 06:14:24 +0200

    social_app

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,14 @@ import 'package:social_app/layout/cubit/cubit.dart';
 import 'package:social_app/layout/social_layout.dart';
 import 'package:social_app/modules/login/login_screen.dart';
 import 'package:social_app/shared/bloc_observer.dart';
+import 'package:social_app/shared/components/components.dart';
 import 'package:social_app/shared/network/local/cach_helper.dart';
 import 'package:social_app/shared/styles/themes.dart';
 import 'shared/components/constants.dart';
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  showToast(message: 'on background message', states: ToastStates.SUCCESS);
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,12 +23,15 @@ void main() async {
   debugPrint('token: $token');
   FirebaseMessaging.onMessage.listen((event) {
     debugPrint(event.data.toString());
+    showToast(message: 'on message', states: ToastStates.SUCCESS);
     //debugPrint(event.notification!.body);
   });
   FirebaseMessaging.onMessageOpenedApp.listen((event) {
     debugPrint(event.data.toString());
+    showToast(message: 'on message opened app', states: ToastStates.SUCCESS);
     //debugPrint(event.notification!.body);
   });
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   Bloc.observer = MyBlocObserver();
   Widget? widget;
   uId = await CacheHelper.getData(key: 'uId');
@@ -48,7 +56,9 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider(
           create: (context) {
-            return SocialCubit()..getUserData()..getPosts();
+            return SocialCubit()
+              ..getUserData()
+              ..getPosts();
           },
         ),
       ],
